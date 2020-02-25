2ceb4d3072c4561c5c9183fba28585e21008c527
cjm00n <jasonweng403@gmail.com>
2020-02-25 23:22:53 +0800

    Site updated: 2020-02-25 23:22:53

--- js/script.js
+++ js/script.js
@@ -15,12 +15,12 @@
         didScroll = true;
     });
 
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
+    // setInterval(function() {
+    //     if (didScroll) {
+    //         hasScrolled();
+    //         didScroll = false;
+    //     }
+    // }, 250);
 
     function hasScrolled() {
         var st = $(this).scrollTop();
@@ -76,7 +76,7 @@
 
     if (typeof(moment) === 'function') {
         $('.article-meta time').each(function () {
-            $(this).text(moment($(this).attr('datetime')).fromNow());
+            $(this).text(moment($(this).attr('datetime')).format('ll'));//.fromNow());
         });
     }
 })(jQuery);
